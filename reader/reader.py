fb2914301bf5cda605f4e5a521c9adf9c2bec925
Mitch LeBlanc <supermitch@gmail.com>
2011-11-14 15:32:31 -0800

    Added sample .csv file and started reader program
    Started modifying our sample CSV file so I could
    test it with the reader program. Started the
    beginnings of a reader program, reader.py

---
+++ reader/reader.py
@@ -0,0 +1,10 @@
+# Reader file created to read a .csv file from excel and assign contents
+# to an object
+
+# Actual data transfer to db would happen elsewhere.
+
+print ("Hello world!")
+
+print ("This is a CSV file uploader. It will upload your files and automagically
+        store the data into a database.")
+
