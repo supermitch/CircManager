383aa764ea207368f82399f81e4cc6ec89fdd44c
Mitch LeBlanc <supermitch@gmail.com>
2011-11-14 16:51:51 -0800

    Updated reader.py and created a test.csv file
    Update reader to include a raw_input entry for file name.
    Also reads and prints file contents.

--- reader/reader.py
+++ reader/reader.py
@@ -3,8 +3,34 @@
 
 # Actual data transfer to db would happen elsewhere.
 
-print ("Hello world!")
+import argparse # access to optional commandline arguments
+# Haven't added this yet!
 
-print ("This is a CSV file uploader. It will upload your files and automagically
-        store the data into a database.")
+import os.path  # access to isfile
 
+print "Hello world!"
+
+print """
+This is a CSV file uploader. It will upload your files and automagically
+store the data into a database."""
+
+# todo: alternatively we want to read this as an argument to the file
+
+#if arg_file == "":
+arg_file = raw_input("Enter filename: ")    # get the name of the .csv file
+
+try:
+
+    os.path.isfile(arg_file)
+    
+    infile = open(arg_file, 'r')    # open the .csv file for reading
+        # not exactly sure, is an error catching thing, ensures file is
+        # closed even if an exception occurs
+
+    for line in infile:     # for each line in the infile list
+        print line,         # print line contents
+
+    infile.close()  # Close the csv input file
+
+except IOError as e:
+   print 'Oh dear. File cannot be opened.'
