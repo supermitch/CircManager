7d4ab4a7fba8e1f4279c29e834ce06c564b9c9c4
Mitch LeBlanc <supermitch@gmail.com>
2011-11-14 20:16:54 -0800

    Add optional file argument to reader.py
    Added optional argv, also a few checks on file
    validity, and echoeing of file name during playback.

--- reader/reader.py
+++ reader/reader.py
@@ -1,36 +1,43 @@
 # Reader file created to read a .csv file from excel and assign contents
 # to an object
+#
+# Mitch LeBlanc 2011-11-14
 
-# Actual data transfer to db would happen elsewhere.
-
-import argparse # access to optional commandline arguments
 # Haven't added this yet!
+import argparse # access to optional commandline arguments
+
+import sys  # access to sys.argv
 
 import os.path  # access to isfile
 
-print "Hello world!"
-
 print """
 This is a CSV file uploader. It will upload your files and automagically
-store the data into a database."""
+store the data into a database.\n"""
 
 # todo: alternatively we want to read this as an argument to the file
 
-#if arg_file == "":
-arg_file = raw_input("Enter filename: ")    # get the name of the .csv file
+if len(sys.argv) == 1:
+    arg_file = raw_input("Enter filename: ")    # get the name of the .csv file
+else:
+    arg_file = sys.argv[1]
+
+if not os.path.isfile(arg_file):
+    sys.exit("Not a valid file name!")
+
+if os.path.splitext(arg_file)[1] != ".csv":
+    sys.exit("Not a valid file type!")
 
 try:
-
-    os.path.isfile(arg_file)
-    
     infile = open(arg_file, 'r')    # open the .csv file for reading
+    print("Reading file: %s\n") % arg_file
         # not exactly sure, is an error catching thing, ensures file is
         # closed even if an exception occurs
-
-    for line in infile:     # for each line in the infile list
-        print line,         # print line contents
-
-    infile.close()  # Close the csv input file
-
 except IOError as e:
    print 'Oh dear. File cannot be opened.'
+
+for line in infile:     # for each line in the infile list
+    print line,         # print line contents
+
+infile.close()  # Close the csv input file
+
+
