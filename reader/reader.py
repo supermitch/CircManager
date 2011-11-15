c8ad9b72cc79cadafc61ee79686676cbb5dd39be
Mitch LeBlanc <supermitch@gmail.com>
2011-11-14 20:23:04 -0800

    Comment all lines of code in reader.py

--- reader/reader.py
+++ reader/reader.py
@@ -3,36 +3,38 @@
 #
 # Mitch LeBlanc 2011-11-14
 
-# Haven't added this yet!
-import argparse # access to optional commandline arguments
+# import argparse # access to optional commandline arguments, currently unused
 
-import sys  # access to sys.argv
+import sys  # access to sys.argv, and exit
 
 import os.path  # access to isfile
 
+# A little intro message
 print """
 This is a CSV file uploader. It will upload your files and automagically
 store the data into a database.\n"""
 
 # todo: alternatively we want to read this as an argument to the file
 
-if len(sys.argv) == 1:
-    arg_file = raw_input("Enter filename: ")    # get the name of the .csv file
+if len(sys.argv) == 1:  # There needs to be TWO arguments if filename is there
+    arg_file = raw_input("Enter filename: ")    # get the name of .csv file
 else:
-    arg_file = sys.argv[1]
+    arg_file = sys.argv[1]  # The second argument (index 1) is the file name
 
+# Just a quick check to see if file name is a valid file
 if not os.path.isfile(arg_file):
     sys.exit("Not a valid file name!")
 
+# Check to see if the file is called .csv (though we don't really care)
 if os.path.splitext(arg_file)[1] != ".csv":
     sys.exit("Not a valid file type!")
 
+# Try to open the file, catch the exception if it fails?
 try:
     infile = open(arg_file, 'r')    # open the .csv file for reading
-    print("Reading file: %s\n") % arg_file
-        # not exactly sure, is an error catching thing, ensures file is
-        # closed even if an exception occurs
-except IOError as e:
+    print("Reading file: %s\n") % arg_file  # Echo the file name
+
+except IOError as e:    # if file open failed for whatever reason
    print 'Oh dear. File cannot be opened.'
 
 for line in infile:     # for each line in the infile list
