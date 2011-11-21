6b3763dcd9aaec1a6a544e0699bbe14221fa1a2f
Mitch LeBlanc <supermitch@gmail.com>
2011-11-20 16:58:45 -0800

    Create uploader application, and got halfway there.
    Basically managed to get a form that seems to work and
    doesn't give a bunch of CSRF errors. Got an index that
    works, but upload does nothing at all, yet.

---
+++ uploader/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.template import Context # useless?
+from django.http import HttpResponse
+from django.core.context_processors import csrf # security thing
+from django.template import RequestContext
+
+from models import UploadFileForm # import our upload form model
+
+
+def index(request):
+    context = {}
+    context.update(csrf(request))  
+    return render_to_response('uploader/index.html', context)
+
+def upload_file(request):
+    context = {}
+    context.update(csrf(request))    
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('uploader/success.html', context)
+    else:
+        form = UploadFileForm()
+    return render_to_response('uploader/index.html', {'form': form}, context_instance=RequestContext(request))
+
+def handle_uploaded_file(f):
+    destination = open('c:/name.txt', 'wb+')
+    for chunk in f.chunks():
+        destination.write(chunk)
+    destination.close()
+
+
--- urls.py
+++ urls.py
@@ -15,4 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^subscribe/', 'subs.views.index'),
+    url(r'^upload/', 'uploader.views.index'),
+    url(r'^upload_file/', 'uploader.views.upload_file'),
 )
