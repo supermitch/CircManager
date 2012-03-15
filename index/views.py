9c13ab0e5a930bf6841f78f675ce71f1e23929a1
Mitch LeBlanc <supermitch@gmail.com>
2012-03-14 18:30:15 -0700

    Update index view to display login msg

--- index/views.py
+++ index/views.py
@@ -1,10 +1,10 @@
-# Create your views here.
-
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponseRedirect
-from django.http import HttpResponse
 from django.template import RequestContext
 
-
 def index(request):
-    return render_to_response('index/index.html')
+    # We need to add our request context in order to access
+    # session variables in our templates
+    #return render_to_response('index/index.html')
+    return render_to_response('index/index.html',
+        context_instance=RequestContext(request,))
+
