a986f023e85b02d8564a938b14575c41eafb2ddd
Mitch LeBlanc <supermitch@gmail.com>
2012-04-16 19:06:10 -0700

    Preliminary form & pages for Pull Products

--- subs/forms.py
+++ subs/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from products.models import Product
+from subs.models import Shipment
 
 class SendIssueForm(forms.Form):
 
-    prod_choices = [(prod.id, prod.code)
-                       for prod in Product.objects.all()]
+    prod_choices = [(prod.id, prod.code) for prod in Product.objects.all()]
 
     product = forms.CharField(initial='Choose a product to ship',
                               max_length = 20,
@@ -12,4 +12,8 @@ class SendIssueForm(forms.Form):
 
     notes = forms.CharField(help_text='Add a note for your records.',
                             max_length = 200,
-                            required=False)    
+                            required=False)
+
+class PullProductForm(forms.Form):
+
+    ship_choices = [(ship.id, ship.code) for ship in Shipment.objects.all()]
